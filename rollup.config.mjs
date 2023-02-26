5688efd189ce7838978b781fd7ed350354f436c9
JoostGevaert <jhngevaert@gmail.com>
2023-02-26 18:51:32 +0100

    Make rollup work with minimal example

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -1,11 +1,11 @@
 import resolve from '@rollup/plugin-node-resolve';
 
 export default {
-  input: 'index.js',
+  input: './src/index.js',
   output: [
     {
       format: 'esm',
-      file: 'bundle.js'
+      file: './dist/bundle.js'
     },
   ],
   plugins: [
