dc625c0fed602ceba07df5b2e14c96bafac8800b
JoostGevaert <jhngevaert@gmail.com>
2023-03-28 13:45:09 +0200

    feat: set up routes for BIM app

---
+++ src/app/building/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+type Props = {}
+
+export default function BuildingViewer({}: Props) {
+  return (
+    <div>BuildingViewer</div>
+  )
+}
---
+++ src/app/map/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+type Props = {}
+
+export default function MapViewer({}: Props) {
+  return (
+    <div>MapViewer</div>
+  )
+}
