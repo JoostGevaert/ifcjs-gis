a5ecbbf8de2a01a5c6ab968053f516e58dd4a966
JoostGevaert <jhngevaert@gmail.com>
2023-04-14 11:27:01 +0200

    add tailwindcss

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,10 @@
 import "./globals.css";
 
+export const metadata = {
+  title: "IFC.js Frontend BIM App",
+  description: "First steps towards a frontend BIM app with IFC.js",
+};
+
 export default function RootLayout({
   children,
 }: {
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,7 +6,13 @@ export const metadata = {
 export default function Home() {
   return (
     <main>
-      <h1>Home</h1>
+      <h1 className="uppercase text-3xl font-bold underline">Home</h1>
+      <p className="m-2 text-mybrown-50 font-bold">
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Cum a, quos
+        ratione ipsa, sequi aperiam aut veritatis officiis necessitatibus
+        praesentium nam repudiandae sit accusamus, porro totam atque. In,
+        reprehenderit veniam.
+      </p>
     </main>
   );
 }
