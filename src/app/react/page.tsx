81ee727ee865444a19d9f0388a99c5a5bb758ef8
JoostGevaert <jhngevaert@gmail.com>
2023-04-15 12:42:50 +0200

    feat: make react page a little nicer

--- src/app/react/page.tsx
+++ src/app/react/page.tsx
@@ -15,22 +15,42 @@ export const metadata = {
 export default function React() {
   return (
     <ContextWrapper>
-      <HelloH1 name="Joost Gevaert" />
-      <div className="flex justify-between">
-        <div className="max-w-prose mx-auto">
-          <section>
-            <HelloFriends />
-          </section>
-          <section>
-            <HelloListAPI userCount={7} />
-          </section>
+      <div className="mx-auto max-w-screen-xl">
+        <HelloH1 name="Joost Gevaert" />
+        <div className="flex justify-around space-x-4">
+          <div className="max-w-prose">
+            <section>
+              <HelloListAPI userCount={7} />
+            </section>
+            <section>
+              <h3 className="mt-2">Some useless content</h3>
+              <p className="text-justify">
+                Lorem ipsum dolor sit amet consectetur, adipisicing elit. Est ea
+                ipsa tempore ipsum eveniet facilis maxime nisi laboriosam
+                repellendus nobis? Adipisci culpa impedit ipsa dignissimos
+                repudiandae. Quasi quos in numquam voluptatum eligendi quod
+                expedita? Dolorem, architecto ut quibusdam facere quisquam esse
+                quo sint ullam dolor eum error asperiores natus, velit nesciunt
+                officiis iure ex possimus perferendis voluptatem sapiente
+                ratione. Earum nostrum iure mollitia? Tempore, animi.
+                Repudiandae distinctio dignissimos placeat, est facere maxime
+                hic ullam dolores ducimus voluptate dolor. At minima provident
+                labore explicabo animi, esse exercitationem amet maiores velit?
+                Placeat, sapiente eius. Possimus quasi enim illum tempore magni
+                architecto nesciunt!
+              </p>
+            </section>
+            <section>
+              <HelloFriends />
+            </section>
+          </div>
+          <aside className="self-center flex flex-col space-y-3 items-center">
+            <HelloButton name="Joop Gevaar" />
+            <HelloCheckbox />
+            <HelloCounter />
+            <HelloRef />
+          </aside>
         </div>
-        <aside className="self-center flex flex-col space-y-3 items-center">
-          <HelloButton name="Joop Gevaar" />
-          <HelloCheckbox />
-          <HelloCounter />
-          <HelloRef />
-        </aside>
       </div>
     </ContextWrapper>
   );
