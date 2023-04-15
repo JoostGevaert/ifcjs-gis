4a405fbaf1b2234d11ab93cefb8919ddcb2ff6b1
JoostGevaert <jhngevaert@gmail.com>
2023-04-15 06:31:10 +0200

    fix: ' -> &apos;

--- src/app/react/components/HelloFriends.tsx
+++ src/app/react/components/HelloFriends.tsx
@@ -2,7 +2,7 @@ export default function HelloFriends() {
   const friends = ["Jeroen", "Siegert", "Jos", "Johannes", "Billy"];
   return (
     <>
-      <h3 className="mt-2">Some of Joost's friends</h3>
+      <h3 className="mt-2">Some of Joost&apos;s friends</h3>
       <ul className="space-y-0.5 list-disc list-inside">
         {friends.map((name, i) => (
           <li key={`${name}${i}`}>{name}</li>
