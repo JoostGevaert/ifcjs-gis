39e35c43e63c6bd99b060b731651be146d4508c7
JoostGevaert <jhngevaert@gmail.com>
2023-04-15 11:31:33 +0200

    chore: last update to next.config.js

--- next.config.js
+++ next.config.js
@@ -1,23 +1,11 @@
 /**
  * @type {import('next').NextConfig}
  */
-// const isGithubActions = process.env.GITHUB_ACTIONS || false;
-
-// let assetPrefix = "";
-// let basePath = "";
-
-// if (isGithubActions) {
-//   // trim off `<owner>/`
-//   const repo = process.env.GITHUB_REPOSITORY.replace(/.*?\//, "");
-
-//   assetPrefix = `/${repo}/`;
-//   basePath = `/${repo}`;
-// }
-
 module.exports = {
   experimental: {
     appDir: true,
   },
+  // config for GitHub Pages:
   // output: 'export',
   // basePath: basePath,
   // assetPrefix: assetPrefix,
