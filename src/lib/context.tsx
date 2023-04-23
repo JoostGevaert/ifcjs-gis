f10667bc025366257fd61af06a5ab3705b8dee38
JoostGevaert <jhngevaert@gmail.com>
2023-04-23 20:33:24 +0200

    feat: context + useState. TODO: context + useReducer

--- src/lib/context.tsx
+++ src/lib/context.tsx
@@ -1,16 +1,19 @@
 "use client";
 
-import React, { createContext, useContext } from "react";
+import React, { createContext, useContext, useState } from "react";
 
-interface IContext {
+interface ISharedState {
   name: string
 }
 
-const context = createContext<IContext | null>(null);
+const context = createContext<ISharedState | null>(null);
 
 export function ContextProvider({ children }: { children: React.ReactNode }) {
+  const [name, setName] = useState("Joop Gevaar");
+  
   let sharedState = {
-    name: "Joop Gevaar"
+    name,
+    setName
   };
 
   return (
